261e5d16e3516741039e4945841e9fe04ef3021f
Sofin Rodion <13avokado13@gmail.com>
2023-05-23 12:20:02 +0300

    commit test success

--- src/test/java/PositiveTest.java
+++ src/test/java/PositiveTest.java
@@ -41,7 +41,7 @@ public class PositiveTest {
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Иван");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79999999999");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.className("[button]")).click();
+        driver.findElement(By.className("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         //var actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
